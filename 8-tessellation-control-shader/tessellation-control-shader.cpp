fadd766b2602307e2fb644361391da2b642a570e
Ackou Jens <ackoujens@gmail.com>
2016-08-20 20:40:34 +0200

    Added comprehensive commentary to the tessellation shader

--- 8-tessellation-control-shader/tessellation-control-shader.cpp
+++ 8-tessellation-control-shader/tessellation-control-shader.cpp
@@ -2,12 +2,42 @@
 #include <Vector>
 
 /*
-  Tessellation: process of breaking a high-order primitive (a patch)
+  Tessellation: process of breaking a high-order primitive (a patch) <- NOTE GL_PATCHES later on
   into many smaller, simpler primitives (triangles) for rendering.
 
   OpenGL includes fixed-function configurable tessellation engine
   =  able to break up: quadrilaterals, triangles, lines
    -> large number of: triangles, lines, smaller points
+
+  Each patch is formed from a number of control points
+    number of control points per patch is configurable and set by calling glPatchParameteri()
+    pname set to GL_PATCH_VERTICES, value set to number of control points that will be used to construct each patch
+  -> void glPatchParameteri(GLenum pname, GLint value);
+
+  Default the number of control points per patch = 3
+  When tessellation active, vertex shader runs once / control point
+
+  Tessellation control shader runs in batches on groups of control points
+  where size of each batch = number of vertices per patch
+
+  Vertices are used as control points.
+  Result of the vertex shader is passed in batches to the tessellation control shader as its input.
+
+  Number of control points per patch can be changed
+  such that the number of control points that is output by the tesselation control shader
+  can be different from the number of control points that it consumes
+
+  The number of control points produced by the control shader
+  is set using an output layout qualifier in the control shader source code
+    -> layout (vertices = N) out;
+    : N is number of control points per patch
+
+  Control shader is responsible for calculating the values of the output control points
+  and for setting the tessellation factors for the resulting patch that will be sent to the fixed function tessellation engine.
+
+  Output tessellation factors are written to the gl_TessLevelInner and gl_TessLevelOuter
+  built in output variables where other data passed down the pipeline is written to
+  userdef output vars (out) or the special built in gl_out array as normal
 
   Can be directly consumed by the normal rasterization hardware further down pipeline
 
@@ -64,10 +94,14 @@ public:
         "}                                                                                      \n"
       };
 
-      // Source code tessellation control shader
+      // Source code tessellation control shader (TCS)
+      // takes input from vertex shader
+      // determines the level of tessellation that will be sent to the tessellation engine
+      // generates data to be sent to evaluation shader
       static const GLchar * tessellationControlShaderSource[] =
       {
         "#version 330 core                                                              \n"
+        // Output control points (3 = same as default)
         "layout (vertices = 3) out;                                                     \n"
         "                                                                               \n"
         "void main(void) {                                                              \n"
@@ -77,17 +111,32 @@ public:
         "       gl_TessLevelOuter[1] = 5.0;                                              \n"
         "       gl_TessLevelOuter[2] = 5.0;                                              \n"
         "   }                                                                           \n"
+        // Copy it's input to the output
+        // gl_InvocationID is used to index into the gl_in and gl_out arrays
+        // this var contains the zero-based index of the control point
+        // within the patch being processed
+        // by the current invocation of
+        // the tessellation control shader
         "   gl_out[gl_InvocationID].gl_Position = gl_in[gl_InvocationID].gl_Position;   \n"
         "}                                                                              \n"
       };
 
-      // Source code tessellation evaluation shader
+      // Source code tessellation evaluation shader (TES)
+      // when fixed-function tessellation engine has run
+      // it produces number of output vertices representing the primitives it has generated
+      // -> passed to tessellation evaluation shader
+      // TES runs an invocation for each vertex produced by the tessellator
+      // When tessellation levels are high, the TES could run an extremely large number of times
       static const GLchar * tessellationEvaluationShaderSource[] =
       {
         "#version 330 core                                              \n"
+        // Sets tessellation mode, new vertices should be generated equally spaced, clockwise winded
         "layout (triangles, equal_spacing, cw) in;                      \n"
         "                                                               \n"
         "void main(void) {                                              \n"
+        // Assigns value to gl_Position
+        // gl_TessCoord = barycentric coordinate of the vertex generated by the tessellator
+        // gl_Position member of gl_in array -> matches gl_out in the TCS
         "   gl_Position = (gl_TessCoord.x * gl_in[0].gl_Position) +      \n"
         "                  (gl_TessCoord.y * gl_in[1].gl_Position) +      \n"
         "                  (gl_TessCoord.z * gl_in[2].gl_Position);      \n"
@@ -168,8 +217,7 @@ public:
         // Use the program we created earlier for rendering
         glUseProgram(renderingProgram);
 
-        // Draw one triangle
-        // Enable wireframe mode
+        // Affect everything, Draw outlines
         glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
         // Needed for anything to draw with the tesselation on
