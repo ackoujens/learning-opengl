a684dfa51b2665564afd22441e44346cb2bc74a8
Jens Ackou <makkura@jenss-mbp-2.home>
2016-08-27 19:31:09 +0200

    Make errors be outputted by printf

--- your-engine-name/src/Engine.cpp
+++ your-engine-name/src/Engine.cpp
@@ -31,15 +31,22 @@ void Engine::shutdown() {}
 void Engine::run(Engine *app) {
     using namespace std;
     // GLFW Init
-    if (!glfwInit()) cout << "GLFW initialization failed !" << endl;
+    if (!glfwInit()) fprintf( stderr, "GLFW initialization failed !\n");
     else cout << "GLFW initialized" << endl;
 
     init();
 
+    // 4x antialiasing
+    glfwWindowHint(GLFW_SAMPLES, 4);
+
     // Enabled OSX to use a more advanced OpenGL Version
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+    // For MacOS
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // Don't use old OpenGL
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
     // Create Window
@@ -48,9 +55,17 @@ void Engine::run(Engine *app) {
                                     this->title,
                                     NULL,
                                     NULL);
-    if (!window) { cout << "Window or OpenGL context creation failed !" << endl; }
+    if (!window) { fprintf(stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try 2.1 version of the tutorials.\n" ); glfwTerminate(); }
     else { cout << "Window or OpenGL context created" << endl; }
     glfwMakeContextCurrent(this->window);
+
+    // If I ever use GLEW
+    /*
+    glewExperimental=true; // needed in core profile
+    if (glewInit() != GLEW_OK) {
+    fprintf(stderr, "Failed to initialize GLEW\n");
+    return -1;
+  } */
 
     startup();
     cout << "Running " << title << " ..." << endl;
